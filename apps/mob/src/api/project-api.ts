416be36446a67ebbde90fcef822e7e7d4d22ec1e
Krishna <krishnachaudhary2007gt@gmail.com>
2026-06-30 11:17:53 +0530

    feat: implement project API functions for creating and retrieving projects

---
+++ apps/mob/src/api/project-api.ts
@@ -0,0 +1,49 @@
+import {API_URL} from "../constants/env";
+
+export async function createProject(name: string , token : string) {
+    const res = await fetch(API_URL + "/projects", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization": "Bearer " + token
+        },
+        body : JSON.stringify({name : name})
+    })
+    const data = await res.json();
+
+    if(!res.json) {
+        throw new Error(data.error || "create falied")
+    }
+    return data;
+}
+
+export async function getProjectsId(id : number,token : string) {
+    const res = await fetch(API_URL + "/projects/" + id , {
+        method : "GET",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization": "Bearer " + token
+        },
+        
+    })
+    const data = await res.json();
+    if(!res.ok) {
+        throw new Error(data.error || "create failed")
+    }
+    return data;
+}
+
+export async function getProjects(token : any) {
+    const res = await fetch(API_URL + "/projects/", {
+        method : "GET",
+        headers : {
+            "Authorization" : "Bearer " + token 
+        }
+    })
+    const data = await res.json()
+    if(!res.ok) {
+        throw new Error(data.error || "falied retry" )
+    }
+    return data;
+}
+
